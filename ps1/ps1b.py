c627be0aaa62d6439edc26d9442beb23bb294f04
floweredscarf <2460753970@qq.com>
2025-04-16 14:58:31 +0000

    feat: complete ps1

--- ps1/ps1b.py
+++ ps1/ps1b.py
@@ -6,14 +6,30 @@
 ##########################################################################################
 ## Get user input for yearly_salary, portion_saved, cost_of_dream_home, semi_annual_raise below ##
 ##########################################################################################
+yearly_salary = float(input("Enter your yearly_salary: "))
+portion_saved = float(input("Enter the percentage of your salary to save, as a decimal: "))
+cost_of_dream_home = float(input("Enter the cost of your dream home: "))
+semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal: "))
 
 
 #########################################################################
 ## Initialize other variables you need (if any) for your program below ##
 #########################################################################
+portion_down_payment = 0.25
+amount_saved = 0
+r = 0.05
+months = 0
 
 
 ###############################################################################################
 ## Determine how many months it would take to get the down payment for your dream home below ## 
 ###############################################################################################
+while amount_saved < cost_of_dream_home * portion_down_payment:
+    amount_saved += amount_saved * (r / 12)
+    amount_saved += (yearly_salary / 12) * portion_saved
+    months += 1
+    if (months % 6 == 0):
+        yearly_salary += yearly_salary * semi_annual_raise
+    
+print(f"Number of months: {months}")
 
--- ps1/ps1c.py
+++ ps1/ps1c.py
@@ -6,14 +6,40 @@
 ##############################################
 ## Get user input for initial_deposit below ##
 ##############################################
+initial_deposit = float(input("Enter the initial deposit: "))
 
 
 #########################################################################
 ## Initialize other variables you need (if any) for your program below ##
 #########################################################################
+cost_of_house = 800000
+portion_down_payment = 0.25
+months = 36
 
 
 ##################################################################################################
 ## Determine the lowest rate of return needed to get the down payment for your dream home below ##
 ##################################################################################################
+if initial_deposit >= cost_of_house * portion_down_payment - 100:
+    r = 0.0
+    steps = 1
+elif initial_deposit * (1 + 1/12) ** months <= cost_of_house * portion_down_payment - 100:
+    r = None
+    steps = 0
+else:
+    low = 0
+    high = 1
+    r = (low + high) / 2.0
+    amount_saved = initial_deposit * (1 + r/12) ** months
+    steps = 1
+    while abs(amount_saved - cost_of_house * portion_down_payment) >= 100:
+        if amount_saved - cost_of_house * portion_down_payment >= 100:
+            high = r
+        else :
+            low = r
+        r = (low + high) / 2.0
+        amount_saved = initial_deposit * (1 + r/12) ** months
+        steps += 1
+print(f"Best savings rate: {r} [or very close to this number]")
+print(f"Step in bisection: {steps}")
 
